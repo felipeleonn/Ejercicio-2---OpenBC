0a5c1f284f331eeb99da73dc757bb1c5158cd394
Felipe Leon <105251386+felipeleonn@users.noreply.github.com>
2022-11-21 01:32:38 -0300

    Add files via upload

---
+++ Ejercicio2/lista.js
@@ -0,0 +1,10 @@
+const lista = ["Felipe", 24, false, "1998/05/29"];
+const libro = {
+  titulo: "Homo Deus",
+  autor: "Yuval Noah Harari",
+  año: 2012,
+  url: "https://www.penguinlibros.com/ar/content/242-yuval-noah-harari?gclid=Cj0KCQiAveebBhD_ARIsAFaAvrEITyzq2vb0JP6pGfpKVCS6lI2W6rscvci67Bi8Z7HASMRgsGSH1M4aAtyPEALw_wcB"
+}
+
+
+console.log(lista, libro);  
